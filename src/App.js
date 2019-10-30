0255a8f9936e6f384c4e5f58d516886c536506d8
dl539994gmail.com <dl539994gmail.com>
2019-10-30 17:58:50 -0300

    adicionar css da nav e botao cadastra

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter} from 'react-router-dom';
 import './App.scss';
+import Nav from './Componentes/Home/nav/nav'
 
 const App = () => {
   return (
     <BrowserRouter>
-      
+      <Nav/>
     </BrowserRouter>
   )
 }
