c2357ff029b6569c491b86501d718becb527c6b6
Heitor <heitorbc3@gmail.com>
2019-10-31 14:27:16 -0300

    Adicionar menu hamburguer e arrumar a nav

---
+++ src/componentes/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Home.scss'
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <header></header>
+            <div>
+            </div>
+        </div>
+    )
+}
+export default Home;
