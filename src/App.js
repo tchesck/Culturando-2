f52fcae6adeafaa39c12330afc9af67edf4a1b9d
Danilo Owens <daniloleandro1011@gmail.com>
2019-11-04 17:59:42 -0300

    adicionar e atualizar site para mobile e colocar os cursos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter} from 'react-router-dom';
 import Navbar from './componentes/Nav/Navbar.js';
 import Home from './componentes/Home/Home.js';
 import Section from './componentes/Section/Section.js';
+import Footer from './componentes/Section/Section.js';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Home />
       <Section />
+      <Footer />
       
     </BrowserRouter>
   )
---
+++ src/componentes/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Footer.scss';
+
+const Footer = () => {
+    return ( 
+        <div className="PP  ">
+        <footer className="Footer">
+
+        </footer>
+        </div>
+    )
+}
+export default Footer;
