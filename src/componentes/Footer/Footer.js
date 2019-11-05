29b95f43fa7d2628eb29eec53cc5904e38f84b8b
Danilo Owens <daniloleandro1011@gmail.com>
2019-11-05 15:37:57 -0300

    Adicionar nome do projeto footer

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -2,22 +2,21 @@ import React from 'react';
 import './Footer.scss';
 import face from '../../imagem/facebook.png';
 import insta from '../../imagem/instagram.png';
-import yout from '../../imagem/youtube.png'
+import yout from '../../imagem/youtube.png';
+import culturando from '../../imagem/cultura.png'
 
 
 const Footer = () => {
     return ( 
         <div className="PP  ">
-            <div className="linha">
-                <p>
-    
-                </p>
-
+            <div className="Nome"> 
+                <img src={culturando} alt="Nome do projeto"></img>
             </div>
+            <div className="linha"></div>
             <div className="icones"> 
-                <img src={face} alt="kjsbf"></img>
-                <img src={insta} alt="kjsbf"></img>
-                <img className="youtube" src={yout} alt="kjsbf"></img>
+                <img src={face} alt="Facebook"></img>
+                <img src={insta} alt="Instagram"></img>
+                <img className="youtube" src={yout} alt="Youtube"></img>
             </div>
         </div>
 
